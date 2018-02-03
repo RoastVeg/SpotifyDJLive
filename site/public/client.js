bb725cd1bebbbeb968d2a8d0a8a4016a77658cee
roastveg <roastveg@hotmail.co.uk>
2018-02-03 14:37:08 +0000

    Access token is returned with every sync on both dj and listener

--- site/public/client.js
+++ site/public/client.js
@@ -1,15 +1,31 @@
 const http = new XMLHttpRequest();
 var intervalId;
+var access_token = getCookie("access_token");
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 function listenSync() {
     http.open("POST", "listen");
-    http.send();
+    http.send(access_token);
     http.onload = () => console.log("beep");
 }
 
 function djSync() {
     http.open("POST", "dj");
-    http.send();
+    http.send(access_token);
     http.onload = () => console.log("boop");
 }
 
