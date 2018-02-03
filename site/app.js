f25430a95f99d608261e5bd5170265da93a0758d
Mark Sheppard <ms14979@bristol.ac.uk>
2018-02-03 13:49:36 +0000

    Spotify login working

--- site/app.js
+++ site/app.js
@@ -1,6 +1,69 @@
+var querystring = require('querystring');
+var request     = require('request');
+
 const SPOTIFY_CLIENT_ID = '33148af9475e489393ea51a5221dc6dd';
 const SPOTIFY_CLIENT_SECRET = 'ad6063329f8046df9709f8ca6c87f791';
+const SPOTIFY_REDIRECT_URI = 'http://localhost:8080/login-callback';
 const SPOTIFY_SCOPE = '';
+
+var spotify_tokens = { };
+
+module.exports = {
+    login,
+    loginCallback,
+    room,
+    dj,
+    listen,
+};
+
+function randomString(length) {
+    var text    = "";
+    var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var ii = 0; ii < length; ii++)
+        text = text + charset.charAt(Math.floor(Math.random() * charset.length));
+
+    return text;
+}
+
+function login(req, resp) {
+    const state = randomString(16);
+
+    resp.redirect('https://accounts.spotify.com/authorize?' + querystring.stringify({
+        response_type: 'code',
+        client_id:     SPOTIFY_CLIENT_ID,
+        scope:         SPOTIFY_SCOPE,
+        redirect_uri:  SPOTIFY_REDIRECT_URI,
+        state:         state
+    }));
+}
+
+function loginCallback(req, resp) {
+    const authOptions = {
+        url:  'https://accounts.spotify.com/api/token',
+        form: {
+            code: req.query.code,
+            redirect_uri: SPOTIFY_REDIRECT_URI,
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET).toString('base64'))
+        },
+        json: true
+    };
+
+    request.post(authOptions, function(error, respTokens, body) {
+        spotify_tokens.access_token = body.access_token;
+        spotify_tokens.refresh_token = body.refresh_token;
+
+        resp.redirect('/room');
+    });
+}
+
+function room(req, resp) {
+    resp.write('<p>Welcome to the room!<p>');
+    resp.end();
+}
 
 function dj(req, resp) {
     resp.write('<p>Hello, DJ!</p>');
@@ -11,8 +74,3 @@ function listen(req, resp) {
     resp.write('<p>Hello, listener!</p>');
     resp.end();
 }
-
-module.exports = {
-    dj,
-    listen,
-};
--- site/server.js
+++ site/server.js
@@ -6,6 +6,9 @@ var expressApp = express();
 
 expressApp.use(express.static(path.join(__dirname, 'public/')))
 
+expressApp.get('/login', app.login);
+expressApp.get('/login-callback', app.loginCallback);
+expressApp.get('/room', app.room);
 expressApp.get('/dj', app.dj);
 expressApp.get('/listen', app.listen);
 
